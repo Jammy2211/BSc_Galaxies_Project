606ef59b5ae622369840970ccfab31ff20fe9f6a
James Nightingale <james.w.nightingale@durham.ac.uk>
2024-12-06 14:33:37 +0000

    notebooks dleete

--- scripts/task_1_structure.py
+++ scripts/task_1_structure.py
@@ -1,4 +1,49 @@
 """
+Project Setup
+=============
+
+At the beginning of the `tutorial.ipynb` notebook, steps were performed to ensure auto-saving of progress,
+installation of the Python libraries used by the project and downloading of the GitHub repository.
+
+These steps are now repeated in this task notebook, make sure you run them before proceeding with the task!
+
+Project Setup: Colab Setup
+==========================
+
+To set up Google Colab for the research project, follow these steps to ensure your progress is saved. These
+instructions are also detailed in the accompanying document, which includes images for guidance.
+
+First, connect Google Colab to your Google account and Google Drive. This setup ensures your work is autosaved and
+preserved even if you close your browser tab or window. Next, open the tutorial notebook in Colab, click on
+the "File" tab in the top-left corner, and select "Save a copy in Drive." This action will create a duplicate of the
+notebook and open it in a new tab titled "Copy of task_filename.ipynb."
+
+In the new tab, rename the notebook by clicking on its title in the top-left corner. Change it
+from "Copy of task_filename.ipynb" to "YOUR_NAME_task_filename.ipynb," replacing "YOUR_NAME" with your actual name.
+With these steps complete, the notebook will now autosave your changes to your Google Drive, ensuring your
+progress is retained.
+
+Project Setup: Software Installation
+====================================
+
+Next, install the Python software libraries required for this research project. In Google Colab, this can be done
+easily by running the cell below in the Jupyter Notebook.
+
+If prompted to restart the session after installation, do so and then rerun the cell to ensure all libraries are
+properly installed.
+"""
+!pip install autogalaxy
+!pip install numba
+
+"""
+Project Setup: Repository Clone
+===============================
+
+The code below retrieves the project files from the GitHub repository.
+"""
+!git clone https://github.com/Jammy2211/BSc_Galaxies_Project
+
+"""
 Task 1: Structure
 =================
 
